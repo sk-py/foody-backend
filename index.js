afd8a947a5920223189daf657346f6bae2f09db1
sk-py <shaikh56742@gmail.com>
2024-08-29 06:58:09 +0530

    Root route added for clarity

--- index.js
+++ index.js
@@ -1,19 +1,23 @@
 const express = require("express")
 const ConnectToMongo = require("./connection")
-
 require("dotenv").config()
+
 ConnectToMongo()
+
+const PORT = process.env.PORT || 3000;
 
 const app = express();
 
 app.use(express.json())
+app.use()
 
 const authRoutes = require('./routes/user')
 
-app.use("/api/auth",authRoutes)
+app.use("/api/auth", authRoutes)
 
-
-const PORT = process.env.PORT || 3000;
+app.get("/", (req, res) => {
+    res.send("Hello World!")
+})
 
 app.listen(PORT,() => {
     console.log(`Server is running on port ${PORT} 🔥`);
