630ae285289080fbddcf81bb7e71d8730ba9197e
sk-py <shaikh56742@gmail.com>
2025-03-20 13:10:09 +0530

    CORS added

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require("express")
 const ConnectToMongo = require("./connection")
+const cors = require("cors")
 require("dotenv").config()
 
 ConnectToMongo()
@@ -9,6 +10,7 @@ const PORT = process.env.PORT || 3000;
 const app = express();
 
 app.use(express.json())
+app.use(cors())
 
 
 const authRoutes = require('./routes/user')
