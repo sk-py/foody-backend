213f897df31ce793bee7785ff6b44c4530cbf297
sk-py <shaikh56742@gmail.com>
2025-06-03 19:09:36 +0530

    Mongodb connection function removed

--- connection.js
+++ connection.js
@@ -2,7 +2,7 @@ const mongoose = require("mongoose")
 
 const ConnectToMongo = async () => {
     try {
-        await mongoose.connect(process.env.MONGO_URI)
+        // await mongoose.connect(process.env.MONGO_URI)
         console.log("Database connected successfully");
     } catch (error) {
         console.log(error);
