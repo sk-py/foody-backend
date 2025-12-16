967a4d9047e40f67154fcf5e42b348f030b610e8
sk-py <shaikh56742@gmail.com>
2025-12-16 21:17:31 +0530

    Added email apis

--- controllers/email.js
+++ controllers/email.js
@@ -1,26 +1,89 @@
-const sgMail = require("@sendgrid/mail");
+// // const sgMail = require("@sendgrid/mail");
 
-sgMail.setApiKey(process.env.SENDGRID_API_KEY);
+// // sgMail.setApiKey(process.env.SENDGRID_API_KEY);
+
+// const sendEmail = async (req, res) => {
+//     const { to, subject, body } = req.body;
+
+//     const msg = {
+//         to,
+//         from: "shaikh56742@gmail.com",
+//         subject,
+//         text: body,
+//         // html: `<strong>${body}</strong>`,
+//     }
+//     console.log(msg);
+
+//     try {
+//         await sgMail.send(msg);
+//         res.status(200).json({ message: "Email sent successfully" });
+//     } catch (error) {
+//         res.status(500).json({ message: "Email not sent" });
+//         console.log(error);
+//     }
+// }
+
+// module.exports = { sendEmail };
+
+const nodemailer = require("nodemailer");
+
+const pass = process.env.APP_PASS;
+
+let mailTransporter = nodemailer.createTransport({
+  service: "gmail",
+  auth: {
+    user: "shaikh56742@gmail.com",
+    pass: pass,
+  },
+});
+
+// let mailDetails = {
+//   from: "shaikh56742@gmail.com",
+//   to: "shifakhan122004@gmail.com",
+//   subject: "Test mail",
+//   text: "Hello :)",
+// };
+
+// const send = mailTransporter.sendMail(mailDetails, function (err, data) {
+//   if (err) {
+//     console.log("Error Occurs");
+//   } else {
+//     console.log("Email sent successfully");
+//   }
+// });
 
 const sendEmail = async (req, res) => {
-    const { to, subject, body } = req.body;
+  const { to, subject, body } = req.body;
 
-    const msg = {
-        to,
-        from: "shaikh56742@gmail.com",
-        subject,
-        text: body,
-        // html: `<strong>${body}</strong>`,
-    }
-    console.log(msg);
+  const msg = {
+    to,
+    from: "shaikh56742@gmail.com",
+    subject,
+    text: body,
+    // html: `<strong>${body}</strong>`,
+  };
+  console.log(msg);
 
-    try {
-        await sgMail.send(msg);
-        res.status(200).json({ message: "Email sent successfully" });
-    } catch (error) {
-        res.status(500).json({ message: "Email not sent" });
-        console.log(error);
-    }
-}
+  try {
+    const response = await mailTransporter.sendMail(
+      msg,
+      function (err, data) {
+        if (err) {
+          console.log("Error Occurs");
+        } else {
+          console.log("Email sent successfully");
+          return data;
+        }
+      }
+    );
+
+    console.log(response);
+
+    res.status(200).json({ message: "Email sent successfully" });
+  } catch (error) {
+    res.status(500).json({ message: "Email not sent" });
+    console.log(error);
+  }
+};
 
 module.exports = { sendEmail };
